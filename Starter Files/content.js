c606ef56fe46c44bf8628c7e7d990c5a4140ce31
Devansh Kesan <devanshkesan101@gmail.com>
2024-12-22 23:42:31 +0530

    add button on webpage

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -5,3 +5,21 @@
 // Project Repository URI: https://github.com/
 // Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
 // License: MIT
+
+const bookmarkImageURL = chrome.runtime.getURL("assets/bookmark.png");
+
+window.addEventListener('load', addBookmarkButton);
+
+function addBookmarkButton() {
+    const bookmarkButton = document.createElement('img');
+    bookmarkButton.id = "add-bookmark-button";
+    bookmarkButton.src = bookmarkImageURL;
+    bookmarkButton.style.height = "30px";
+    bookmarkButton.style.width = "30px";
+
+    const askdoubtbutton = document.getElementsByClassName("coding_ask_doubt_button__FjwXJ")[0]
+
+    askdoubtbutton.parentNode.insertAdjacentElement("afterend",bookmarkButton);
+
+
+}
