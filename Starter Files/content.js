84baece6eeda23901c73a8a0ac1a9414eb359150
Devansh Kesan <devanshkesan101@gmail.com>
2024-12-23 17:01:38 +0530

    add extension popup and functionality

--- Starter Files/content.js
+++ Starter Files/content.js
@@ -7,10 +7,27 @@
 // License: MIT
 
 const bookmarkImageURL = chrome.runtime.getURL("assets/bookmark.png");
+const AZ_PROBLEM_KEY = "AZ_PROBLEM_KEY";
 
-window.addEventListener('load', addBookmarkButton);
+
+// window.addEventListener('load', addBookmarkButton);
+
+const observer = new MutationObserver(() => {
+    addBookmarkButton();
+})
+
+observer.observe(document.body,{childList: true, subtree: true});
+
+addBookmarkButton();
+
+function onProblemsPage(){
+    return window.location.pathname.startsWith("/problems/");
+}
 
 function addBookmarkButton() {
+    console.log("Trigerring ! ");
+    if(!onProblemsPage() || document.getElementById("add-bookmark-button")) return ;
+
     const bookmarkButton = document.createElement('img');
     bookmarkButton.id = "add-bookmark-button";
     bookmarkButton.src = bookmarkImageURL;
@@ -21,5 +38,46 @@ function addBookmarkButton() {
 
     askdoubtbutton.parentNode.insertAdjacentElement("afterend",bookmarkButton);
 
+    bookmarkButton.addEventListener("click", addNewBookmarkHandler);
 
+}
+
+async function addNewBookmarkHandler() {
+    const getCurrentBookmarks = await getCurrentBookmark();
+
+    const azProblemUrl = window.location.href;
+    const uniqueId = extractUniqueId(azProblemUrl);
+    const problemName = document.getElementsByClassName("Header_resource_heading__cpRp1")[0].innerHTML;
+    
+    if(getCurrentBookmarks.some((bookmark) => bookmark.id === uniqueId)) return;
+
+
+    const BookmarkObj = {
+        id: uniqueId,
+        name: problemName,
+        url: azProblemUrl
+    }
+
+    const updatedBookmarks = [...getCurrentBookmarks,BookmarkObj];
+
+    chrome.storage.sync.set({AZ_PROBLEM_KEY: updatedBookmarks}, () => {
+        console.log("Updated the bookmarks correctly to ",updatedBookmarks);
+    })
+
+}
+
+function extractUniqueId(url) {
+    const start = url.indexOf("problems/") + "problems/".length;
+    const end = url.indexOf("?",start);
+    return end == -1 ? url.substring(start) : url.substring(start,end);
+}
+
+
+function getCurrentBookmark() {
+    return new Promise((resolve,reject) => {
+        chrome.storage.sync.get([AZ_PROBLEM_KEY],(results) => {
+            resolve(results[AZ_PROBLEM_KEY] || []);
+        });
+    })
+    
 }
